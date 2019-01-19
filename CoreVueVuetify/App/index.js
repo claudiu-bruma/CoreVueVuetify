d3e226d2b715c993f9a2fd8fad02ae59bc72dd5e
claudiu buma <claudiu.bruma@gmail.com>
2019-01-20 00:03:44 +0200

    got vue up and running

---
+++ CoreVueVuetify/App/index.js
@@ -0,0 +1,25 @@
+﻿import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+console.log('starting vue initialization');
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        component: App
+    }
+];
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
+
+new Vue({
+    el: '#app',
+    template: "<div class='test'><router-view></router-view></div>",
+    router
+});
+console.log('end vue initialization')
